7d463984cf354149c489fa2d05317eef76729fab
etienne <iopzae@gmail.com>
2017-07-01 21:21:00 +0200

    add dataframe funct.

--- sunradiation.py
+++ sunradiation.py
@@ -1,6 +1,7 @@
-"""  Get the sun position + solar flux
+"""  Get the sun position and the solar flux
      Projected on a surface
      
+     en utilisant la librairie Pysolar
 """
 
 import numpy as np
@@ -15,7 +16,9 @@ import pysolar.radiation as radiation
 import pysolar.solar as solar
 
 
-# Profil de l'horizon :
+""" Prend en compte le masquage par l'horizon 
+
+"""
 horizon_data = genfromtxt('horizon.csv', delimiter=',').T
 
 
@@ -41,6 +44,7 @@ def get_flux_surface( coords, date, sigma, phi_C ):
     phi_C : deg, azimuth, relative to south, with positive values
             in the southeast direction and negative values in the southwest
     
+    return: flux solaire (W/m2)
     """
     
     # Sun position
@@ -50,6 +54,7 @@ def get_flux_surface( coords, date, sigma, phi_C ):
     I0 = radiation.get_radiation_direct( date, beta_deg )   # W/m2
     I0 = I0* isUpperHorizon( phi_S_deg, beta_deg )
     
+    # Projection: 
     beta = beta_deg*math.pi/180  # rad
     phi_S = phi_S_deg*math.pi/180  #rad
     sigma = sigma*math.pi/180
@@ -73,3 +78,85 @@ def get_flux_total( coords, date ):
     I0 = get_radiation_direct( d, beta_deg )   # W/m2
         
     return I0
+  
+  
+  
+def sun_positionAndFlux( coords, date ):
+    """ Obtient la position et le flux solaire pour une date particulière
+        coords: tuple gps
+        date: datetime object
+        
+        return: ( flux solaire (W/m2), sunAzimuth, sunAltitude )
+    """
+    
+    # Sun position
+    phi_S_deg = solar.get_azimuth( *coords, date ) # deg, azimuth of the sun,relative to south
+    beta_deg = solar.get_altitude( *coords, date ) # deg, altitude angle of the sun
+    
+    I0 = radiation.get_radiation_direct( date, beta_deg )   # W/m2
+    I0 = I0* isUpperHorizon( phi_S_deg, beta_deg )
+    
+    
+    return (I0, phi_S_deg, beta_deg)
+
+    
+def buildmultidayDF(  coords, index, cloudCover=None ):
+    """ Construit un dataframe avec le flux solaire, et la position du soleil
+        index: pandas dataframe index
+        coords: tuple, GPS coords
+        
+        return a dataframe
+    """
+
+    array = np.array( [ sun_positionAndFlux( coords, d ) for d in index  ] )
+    
+    cols = ['I0', 'sunAzimuth', 'sunAltitude']
+    
+    df = pd.DataFrame(data=array, index=index, columns=cols ) 
+    
+    if isinstance(cloudCover, pd.Series): 
+        df['I0'] = df['I0']*( 1 - cloudCover )
+        
+    return df 
+    
+def project( sigma, phi_C, I0, phi_S_deg, beta_deg ):
+    """ Calcul le flux solaire vu par une surface ayant une certaine orientation
+        
+        ref.: http://www.a-ghadimi.com/files/Courses/Renewable%20Energy/REN_Book.pdf
+        book "Renewable and Efficient Electric Power Systems", Gilbert M. Masters, 2004
+        page 414
+        
+        sigma:  angle de la surface avec l'horizontal, deg
+        phi_C: azimuth de la surface, deg
+        
+        I0: flux solaire
+        phi_S: azimuth du soleil, deg
+        beta: altitude du soleil, deg
+    """
+    
+    # conversion en radian: 
+    sigma = sigma*math.pi/180
+    phi_C = phi_C*math.pi/180
+    
+    beta = beta_deg*math.pi/180  # rad
+    phi_S = phi_S_deg*math.pi/180  #rad
+
+    
+    cosTheta = math.cos(beta)*math.cos( phi_S - phi_C )*math.sin( sigma ) \
+                + math.cos( sigma )*math.sin( beta )
+    
+    if cosTheta >0 :
+        Isurf = I0*cosTheta   # flux projeté, W/m2
+    else:
+        Isurf = 0  # mais diffuse... ?
+        
+    return Isurf
+    
+def projectDF( sigma, phi_C, sundata):    
+    """ build a dataframe with flux on a surface
+    """
+    get_fluxsurface = lambda sundata, sigma, phi_C: project( sigma, phi_C, *sundata.values )
+    df = sundata.apply( get_fluxsurface, axis=1, sigma=sigma, phi_C=phi_C )
+    return df
+    
+    return df
